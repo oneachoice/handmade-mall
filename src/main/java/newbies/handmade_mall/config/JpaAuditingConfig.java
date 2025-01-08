13a0050211d419b2be7b4c79c9786b17600dec33
oneachoice <oneachoice@gmail.com>
2025-01-08 21:33:59 +0900

    Feat: JpaAuditingConfig 클래스 추가
    Spring Data JPA에서 엔티티의 Auditing 기능을 활성화합니다.

---
+++ src/main/java/newbies/handmade_mall/config/JpaAuditingConfig.java
@@ -0,0 +1,12 @@
+package newbies.handmade_mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Spring Data JPA에서 Auditing 기능을 활성화하기 위한 설정
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
